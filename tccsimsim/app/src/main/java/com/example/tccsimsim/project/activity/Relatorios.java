4fc17626488ec2a664a8b52b095a0d412b4716bc
lipereichert94 <56693315+lipereichert94@users.noreply.github.com>
2020-06-07 17:52:33 -0300

    relatorio ok
    relatorio ok

--- tccsimsim/app/src/main/java/com/example/tccsimsim/project/activity/Relatorios.java
+++ tccsimsim/app/src/main/java/com/example/tccsimsim/project/activity/Relatorios.java
@@ -24,7 +24,15 @@ import androidx.fragment.app.Fragment;
 
 import com.example.tccsimsim.R;
 import com.example.tccsimsim.project.banco.BDSQLiteHelper;
+import com.example.tccsimsim.project.model.AI;
+import com.example.tccsimsim.project.model.Analise_Laboratorial;
+import com.example.tccsimsim.project.model.Atestado_Saude;
 import com.example.tccsimsim.project.model.Estabelecimento;
+import com.example.tccsimsim.project.model.Licenca_Ambiental;
+import com.example.tccsimsim.project.model.Media_Mensal;
+import com.example.tccsimsim.project.model.Produto;
+import com.example.tccsimsim.project.model.RNC;
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
@@ -41,6 +49,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
 import java.util.List;
 
 public class Relatorios extends Fragment implements View.OnClickListener {
@@ -126,34 +137,206 @@ public class Relatorios extends Fragment implements View.OnClickListener {
 
         PdfWriter writer = PdfWriter.getInstance(document,outputStream);
 
-        writer.setBoxSize("tccsimsim", new Rectangle(36,54,559,788));
+       // writer.setBoxSize("tccsimsim", new Rectangle(36,54,559,788));
 
 
         document.open();
 
-        Font font = new Font(Font.FontFamily.HELVETICA,20,Font.BOLD);
+        Font font = new Font(Font.FontFamily.HELVETICA,30,Font.BOLD);
+        Font font_2 = new Font(Font.FontFamily.HELVETICA,20,Font.BOLD);
 
-        Paragraph paragraph = new Paragraph("Curso Firebase Módulo II",font);
+        Paragraph paragraph = new Paragraph("DOCUMENTO PARA AUDITORIA ESTADUAL",font);
 
         paragraph.setAlignment(Element.ALIGN_CENTER);
-
-        Paragraph paragraph_2 = new Paragraph("Jone Arce",font);
-
+        paragraph.setSpacingAfter(10);
+        Paragraph paragraph_2 = new Paragraph("Estabelecimentos cadastrados:",font_2);
         paragraph_2.setAlignment(Element.ALIGN_LEFT);
-
+        paragraph_2.setSpacingAfter(10);
+        document.add(paragraph);
+        document.add(paragraph_2);
 
         ListItem item = new ListItem();
 
-        item.add(paragraph);
-        item.add(paragraph_2);
-
-
-
+        try {
+            List<Estabelecimento> listaestabelecimentos = bd.getAllEstabelecimentos();
+            Estabelecimento estabelecimento;
+            for(int i = 0; i<listaestabelecimentos.size();i++){
+                estabelecimento = listaestabelecimentos.get(i);
+                item.add("Nome: " +estabelecimento.getNome()+"\n");
+                item.add("Nome fantasia: "+estabelecimento.getNome_fantasia()+"\n");
+                item.add("Classificação: "+estabelecimento.getClassificacao()+"\n");
+                item.add("CNPJ: "+estabelecimento.getCnpj()+"\n");
+                item.add("Inscrição Estadual: "+estabelecimento.getInscricao_estadual()+"\n");
+                item.add("Inscrição Municipal: "+estabelecimento.getInscricao_municipal()+"\n");
+                item.add("Endereço: "+estabelecimento.getEndereco()+"\n");
+                item.add("Endereço eletrônico: "+estabelecimento.getEndereco_eletronico()+"\n");
+                item.add("Data de registro: "+estabelecimento.getDt_registro()+"\n");
+                item.add("\n");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         document.add(item);
+        item.clear();
 
+        Paragraph paragraph_3 = new Paragraph("Produtos cadastrados:",font_2);
+        paragraph_3.setAlignment(Element.ALIGN_LEFT);
+        paragraph_3.setSpacingAfter(10);
+        document.add(paragraph_3);
+        try {
+            List<Produto> listaprodutos = bd.getAllProduto();
+            Produto produto;
+            for(int i = 0; i<listaprodutos.size();i++){
+                produto = listaprodutos.get(i);
+                item.add("Nome: " +produto.getNome()+"\n");
+                item.add("Estabelecimento: "+produto.getEstabelecimento().getNome());
+                item.add("\n"+"\n");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        document.add(item);
+        item.clear();
 
+        Paragraph paragraph_4 = new Paragraph("Média mensal cadastradas:",font_2);
+        paragraph_4.setAlignment(Element.ALIGN_LEFT);
+        paragraph_4.setSpacingAfter(10);
+        document.add(paragraph_4);
+        try {
+            List<Media_Mensal> listamediasmensais = bd.getAllMediaMensal();
+            Media_Mensal media_mensal;
+            for(int i = 0; i<listamediasmensais.size();i++){
+                media_mensal = listamediasmensais.get(i);
+                item.add("Estabelecimento: " +media_mensal.getProduto().getEstabelecimento().getNome()+"\n");
+                item.add("Nome do Produto: " +media_mensal.getProduto().getNome()+"\n");
+                item.add("Quantidade: "+media_mensal.getQuantidade()+"\n");
+                item.add("Data mensal da produção: "+media_mensal.getDt_media_mensal());
+                item.add("\n"+"\n");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        document.add(item);
+        item.clear();
 
+        Paragraph paragraph_5 = new Paragraph("Análises Laboratoriais:",font_2);
+        paragraph_5.setAlignment(Element.ALIGN_LEFT);
+        paragraph_5.setSpacingAfter(10);
+        document.add(paragraph_5);
+        try {
+            List<Analise_Laboratorial> listaanaliseslaboratoriais = bd.getAllAnaliseLaboratorial();
+            Analise_Laboratorial analise_laboratorial;
+            for(int i = 0; i<listaanaliseslaboratoriais.size();i++){
+                analise_laboratorial = listaanaliseslaboratoriais.get(i);
+                item.add("Estabelecimento: " +analise_laboratorial.getProduto().getEstabelecimento().getNome()+"\n");
+                item.add("Nome do Produto: " +analise_laboratorial.getProduto().getNome()+"\n");
+                item.add("Tipo da Análise: "+analise_laboratorial.getTipo()+"\n");
+                item.add("Data da Coleta: "+analise_laboratorial.getDt_coleta()+"\n");
+                item.add("Situação: "+analise_laboratorial.getSituacao_coleta()+"\n");
+                item.add("Notificação: "+analise_laboratorial.getNotificacao()+"\n");
+                item.add("Data da Nova Coleta: "+analise_laboratorial.getDt_nova_coleta()+"\n");
+                item.add("Situação da Nova Coleta: "+analise_laboratorial.getDt_nova_coleta());
+                item.add("\n"+"\n");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        document.add(item);
+        item.clear();
 
+        Paragraph paragraph_7 = new Paragraph("Autos de Infração:",font_2);
+        paragraph_7.setAlignment(Element.ALIGN_LEFT);
+        paragraph_7.setSpacingAfter(10);
+        document.add(paragraph_7);
+        try {
+            List<AI> listaai = bd.getAllAI();
+            AI ai;
+            for(int i = 0; i<listaai.size();i++){
+                ai = listaai.get(i);
+                item.add("Estabelecimento: " +ai.getEstabelecimento().getNome()+"\n");
+                item.add("Data: " +ai.getDt_ai()+"\n");
+                item.add("Infração: "+ai.getInfracao_ai()+"\n");
+                item.add("Penalidade: "+ai.getPenalidade_ai()+"\n");
+                item.add("Situação: "+ai.getSituacao_ai());
+                item.add("\n"+"\n");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        document.add(item);
+        item.clear();
+
+        Paragraph paragraph_8 = new Paragraph("Atestado de Saúde:",font_2);
+        paragraph_8.setAlignment(Element.ALIGN_LEFT);
+        paragraph_8.setSpacingAfter(10);
+        document.add(paragraph_8);
+        try {
+            List<Atestado_Saude> listaatestadodesaude = bd.getAllAtestadoSaude();
+            Atestado_Saude atestado_saude;
+            for(int i = 0; i<listaatestadodesaude.size();i++){
+                atestado_saude = listaatestadodesaude.get(i);
+                item.add("Estabelecimento: " +atestado_saude.getEstabelecimento().getNome()+"\n");
+                item.add("Data de Validade: " +atestado_saude.getDt_validade()+"\n");
+                item.add("Data de registro: "+atestado_saude.getDt_registro()+"\n");
+                item.add("\n"+"\n");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        document.add(item);
+        item.clear();
+
+        Paragraph paragraph_9 = new Paragraph("Licença Ambiental:",font_2);
+        paragraph_9.setAlignment(Element.ALIGN_LEFT);
+        paragraph_9.setSpacingAfter(10);
+        document.add(paragraph_9);
+        try {
+            List<Licenca_Ambiental> listalicencaambiental = bd.getAllLicencaAmbiental();
+            Licenca_Ambiental licenca_ambiental;
+            for(int i = 0; i<listalicencaambiental.size();i++){
+                licenca_ambiental = listalicencaambiental.get(i);
+                item.add("Estabelecimento: " +licenca_ambiental.getEstabelecimento().getNome()+"\n");
+                item.add("Data de Validade: " +licenca_ambiental.getDt_validade()+"\n");
+                item.add("Data de registro: "+licenca_ambiental.getDt_registro()+"\n");
+                item.add("\n"+"\n");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        document.add(item);
+        item.clear();
+
+        Paragraph paragraph_6 = new Paragraph("Relatórios de não conformidade:",font_2);
+        paragraph_6.setAlignment(Element.ALIGN_LEFT);
+        paragraph_6.setSpacingAfter(10);
+        document.add(paragraph_6);
+
+        try {
+            List<RNC> listarnc = bd.getAllRNC();
+            RNC rnc;
+            for(int i = 0; i<listarnc.size();i++){
+                rnc = listarnc.get(i);
+                item.add("Estabelecimento: " +rnc.getEstabelecimento().getNome()+"\n");
+                item.add("Data da inspeção: " +rnc.getDt_inspecao()+"\n");
+                item.add("Descrição: " +rnc.getDescricao()+"\n");
+                item.add("Data da verificação: " +rnc.getDt_verificacao()+"\n");
+                item.add("Situação: " +rnc.getSituacao()+"\n");
+                item.add("Imagem: "+"\n"+"\n");
+                Image image = Image.getInstance(rnc.getUrl_imagem());
+                image.scaleAbsolute(200f,300f);
+                image.setAlignment(Element.ALIGN_CENTER);
+                item.add(new Chunk(image,0,0,true));
+                item.add("\n"+"\n");
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        document.add(item);
         document.close();
 
         visualizarPDF(pdf);
